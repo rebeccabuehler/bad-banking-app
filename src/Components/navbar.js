f8d5064b853c9f333657051e40af096e8021582f
Rebecca Buehler <you@example.com>
2022-09-27 14:44:54 -0700

    updated navbar to have tooltips

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -6,15 +6,12 @@ function Navbar() {
       <>
         <nav className="navbar navbar-expand-lg bg-light">
           <div className="container-fluid">
-            <Link className="navbar-brand" aria-current="page" to="/Component/home">
+            <Link className="navbar-brand" aria-current="page" to="/Component/home" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Welcome to Bad Bank!">
               Bad Bank
             </Link>
             <button
               className="navbar-toggler"
               type="button"
-              data-bs-toggle="tooltip"
-              data-bs-placem="top"
-              data-bs-title="Bad Bank"
               data-bs-target="#navbarNav"
               aria-controls="navbarNav"
               aria-expanded="false"
@@ -25,22 +22,22 @@ function Navbar() {
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <Link className="nav-link" to="/Component/createaccount">
+                  <Link className="nav-link" to="/Component/createaccount" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Create Your New Account!">
                     CreateAccount
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/Component/deposit">
+                  <Link className="nav-link" to="/Component/deposit" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Deposit Funds in Your New Account!">
                     Deposit
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/Component/withdraw">
+                  <Link className="nav-link" to="/Component/withdraw" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Withdraw Funds from Your New Account!">
                     Withdraw
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/Component/alldata">
+                  <Link className="nav-link" to="/Component/alldata" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Check out all our data!">
                     AllData
                   </Link>
                 </li>
